da205d83ff65ce8420f9f90948cba98d7493aeac
amada22 <elbouzarimohamed241@gmail.com>
2025-05-13 21:00:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Optional JavaScript for carousel auto-scroll (can be extended if needed)
+const carousel = document.querySelector('.carousel-wrapper');
+let scrollAmount = 0;
+setInterval(() => {
+  scrollAmount += 300;
+  if (scrollAmount >= carousel.scrollWidth) scrollAmount = 0;
+  carousel.scrollTo({ left: scrollAmount, behavior: 'smooth' });
+}, 3000);
